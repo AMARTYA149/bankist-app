546f1e10d6b0ea1c459b77e21102e720568051c4
AMARTYA149 <amartya970914@gmail.com>
2023-01-29 13:22:02 +0530

    chaining methods and showing in, out, interest

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const displayMovements = function (movements) {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
         </div>
     `;
 
@@ -80,6 +80,13 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+
+calcDisplayBalance(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -92,18 +99,113 @@ const createUsernames = function (accs) {
 };
 
 createUsernames(accounts);
-console.log(accounts);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumOut.textContent = `${Math.abs(outcomes)}`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+calcDisplaySummary(account1.movements);
+
+// let movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const eurToUsd = 1.1;
+
+// //PIPELINE
+// const totalDepositsUSD = movements
+//   .filter(mov => mov > 0)
+//   .map((mov, i, arr) => {
+//     // console.log(arr);
+//     return mov * eurToUsd;
+//   })
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// console.log(totalDepositsUSD);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // [5,2,4,1,15,8,3]
+// // [16,6,10,5,6,1,4]
+// const dogArray = [5, 2, 4, 1, 15, 8, 3];
+
+// var calcHumanAge = function () {
+//   return function (age) {
+//     return age <= 2 ? age * 2 : 16 + age * 4;
+//   };
+// };
+
+// // function callPrint() {
+// //   console.log('chkjlc');
+// // }
+
+// // callPrint();
+
+// const filterDogAge = function () {
+//   return function (age) {
+//     return age >= 18 ? true : false;
+//   };
+// };
+
+// function challende(array) {
+//   const result = array
+//     .map(function (age) {
+//       if (age <= 2) {
+//         age *= 2;
+//       } else {
+//         age = 16 + age * 4;
+//       }
+//       return age;
+//     })
+//     .filter(function (age) {
+//       return age < 18 ? false : true;
+//     })
+//     .reduce((acc, item) => {
+//       return (acc += item);
+//     }, 0);
+//   console.log(result);
+//   console.log(`${result / array.length}`);
+// }
+
+// function challende(array) {
+//   const result = array
+//     .map(calcHumanAge())
+//     .filter(filterDogAge())
+//     .reduce((acc, item) => {
+//       return (acc += item);
+//     }, 0);
+//   console.log(result);
+//   console.log(`${result / array.length}`);
+// }
+
+// challende([5, 2, 4, 1, 15, 8, 3]);
 
 /////////////////////////////////////////////////
